9d1658cf637bb4dbf5abce66042dcdfa14489eb2
Ju <jumalleydev@gmail.com>
2025-01-22 18:26:08 +0100

    Update events.lua

--- qb-core/server/events.lua
+++ qb-core/server/events.lua
@@ -18,13 +18,14 @@ AddEventHandler('playerDropped', function(reason)
     QBCore.Players[src] = nil
 end)
 
+--[[
 AddEventHandler("onResourceStop", function(resName)
     for i,v in pairs(QBCore.UsableItems) do
         if v.resource == resName then
             QBCore.UsableItems[i] = nil
         end
     end
-end)
+end)]]
 
 -- Player Connecting
 local readyFunction = MySQL.ready
